fb46eda2a291031a7f1328a149aff37a439b1d46
Mohammad Nafisi <nafisidev@gmail.com>
2023-03-06 20:40:32 -0800

    Adding the text box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='flex justify-center items-center bg-gray-800'>
+      <button className='text-white bg-blue-700 hover:bg-indigo-800 focus:ring-4 font-medium rounded-md text-medium px-4 py-1 m-2'>
+        Button
+      </button>
+      </div>
+      <div className='flex justify-center items-center bg-gray-800'>
+      <button className='text-white bg-indigo-600 hover:bg-indigo-300 focus:ring-4 font-medium rounded-md text-medium px-4 py-1 m-2'>
+        Button
+      </button>
+      </div>
+      <div className='flex justify-center items-center bg-gray-800'>
+      <button className='text-white bg-fuchsia-500 hover:bg-indigo-300 focus:ring-4 font-medium rounded-md text-medium px-4 py-1 m-2'>
+        Button
+      </button>
+      </div>
+      <div className='flex justify-center items-center bg-gray-800'>
+      <button className='text-black bg-yellow-300 hover:bg-indigo-300 focus:ring-4 font-medium rounded-md text-medium px-4 py-1 m-2'>
+        Button
+      </button>
+      </div>
+      <div className='flex justify-center items-center bg-gray-800'>
+      <button className='text-white bg-red-600 hover:bg-indigo-300 focus:ring-4 font-medium rounded-md text-medium px-4 py-1 m-2'>
+        Button
+      </button>
+      </div>
+     
+    
+      <div class="mx-auto flex max-w-screen-sm items-center justify-center rounded-md">
+  <div class="h-36 w-full rounded-md bg-gradient-to-r from-red-600 via-yellow-500 via-fuchsia-500 via-violet-800 to-blue-700 p-1">
+    <div class="flex h-full w-full items-center justify-center bg-slate-700 back">
+      <h1 class="text-2xl font-black text-white">Here is the nice box</h1>
+    </div>
+  </div>
+</div>
+      
     </div>
   );
 }
